cea55478368d075ee24a301d9f52b79459a7ac44
th718 <77204786+th718@users.noreply.github.com>
2023-09-02 02:22:05 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,40 +1,35 @@
 # start(inclusive) end(exclusice)
 # prime number give binary
 # not prime number give array of divisors
-
-start = int(input())
-end = int(input())
-
-
-def prime(num):
-    if num % 2 == 0 or num % 3 == 0:
+#create dict
+#take input from num in range
+def is_prime(num):
+    if(num<2):
         return False
-    if num % 5 == 0 or num % 7 == 0:
-        return False
-    if num < 1:
-        return False
-    if num == 2:
-        return True
-    else:
-        return True
+    for i in range(2,num):
+        if(num%i==0):
+            return False
+    return True
 
-
-def divisors(num):
+def get_divisors(num):
     divisors = []
-    for i in range(1,num+1):
+    for i in range(1, num + 1):
         if num % i == 0:
             divisors.append(i)
-            return divisors
+    return divisors
 
-
-def divisors_or_prime(start, end):
-    dictionary = {}
-
-    for i in range(int(start), int(end)):
-        if prime(num):
-            dictionary[num] = (bin(num))
+def divisors_and_prime(start, end):
+    arrays = {}
+    for num in range(start, end):
+        if is_prime(num):
+            arrays[num] = bin(num)
         else:
-            dictionary[num]=divisors((num))
-            return dictionary
-dictionary_dict=divisors_or_prime(start+end)
-print(dictionary_dict)
+            arrays[num] = get_divisors(num)
+    return arrays
+print("start:")
+start_range = int(input())
+print("end:")
+end_range = int(input())
+
+range_dict = divisors_and_prime(start_range, end_range)
+print(range_dict)
